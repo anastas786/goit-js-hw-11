988283ea723eecd5facd4a67285db045cac48a17
anastas786 <agorbatuk08@gmail.com>
2023-10-17 18:08:37 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,13 @@
-
 import Notiflix from 'notiflix';
 import { performImageSearch } from './api.js';
 
-// Отримання елементів форми та поля вводу
+
 const form = document.getElementById('search-form');
 const input = form.querySelector('input[name="searchQuery"]');
 const imageContainer = document.querySelector('.gallery');
+const loadMoreButton = document.querySelector('.load-more');
 
 
-// Ініціалізація Notiflix
 Notiflix.Notify.init({
     position: 'center-top',
     width: '280px',
@@ -17,32 +16,25 @@ Notiflix.Notify.init({
     background: '#eebf31',
 });
 
-// Функція для показу форми та схову контейнера з зображеннями
+let currentPage = 1;
+const per_page = 40;
+let totalPages = 0;
+let imagesLoaded = 0;
+
+
 function showForm() {
     form.style.display = 'block';
     imageContainer.style.display = 'none';
 }
 
-// При завантаженні сторінки показати форму
-showForm();
-
-let currentPage = 1;
-const loadMoreButton = document.querySelector('.load-more');
-
-// Функція для показу/приховування кнопки "Load more"
-function toggleLoadMoreButton(visible) {
-    loadMoreButton.style.display = visible ? 'block' : 'none';
-}
-
-// Приховати кнопку "Load more" при запуску
 toggleLoadMoreButton(false);
 
-// Функція для очищення галереї
+
 function clearGallery() {
     imageContainer.innerHTML = '';
 }
 
-// Функція для створення та додавання карток зображень в галерею
+
 function createImageCards(images) {
     const imageCards = images.map((image) => {
         return `
@@ -58,60 +50,55 @@ function createImageCards(images) {
         `;
     });
 
-    imageContainer.innerHTML = imageCards.join('');
+    imageContainer.innerHTML += imageCards.join('');
+    imagesLoaded += images.length;
 }
 
 
-let totalHits = 0; // Инициализируем totalHits
-const per_page = 40; // Задаем количество изображений на страницу
-let totalPages = 0; // Инициализируем totalPages
+function toggleLoadMoreButton(visible) {
+    loadMoreButton.style.display = visible ? 'block' : 'none';
+}
 
-// Обработчик формы
+
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
     const searchQuery = input.value.trim();
 
     if (searchQuery) {
         currentPage = 1;
+        imagesLoaded = 0;
         toggleLoadMoreButton(false);
+        clearGallery();
+        showForm();
 
         try {
             const searchData = await performImageSearch(searchQuery, currentPage);
             const images = searchData.hits;
-            totalHits = searchData.totalHits; // Обновляем totalHits
+            totalPages = Math.ceil(searchData.totalHits / per_page);
 
             if (images.length === 0) {
                 Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-                showForm();
             } else {
-                if (currentPage === 1) {
-                    clearGallery();
-                }
-
                 createImageCards(images);
-
                 imageContainer.style.display = 'block';
 
-                totalPages = Math.ceil(totalHits / per_page); // Обновляем totalPages
-
-                if (images.length < 40) {
-                    toggleLoadMoreButton(false);
-                    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
-                } else {
+                if (imagesLoaded < searchData.totalHits) {
                     toggleLoadMoreButton(true);
+                } else {
+                    toggleLoadMoreButton(false);
+                    Notiflix.Notify.info("You've reached the end of search results.");
                 }
             }
         } catch (error) {
             console.error('Error:', error);
             Notiflix.Notify.failure('An error occurred while fetching images. Please try again later.');
-            showForm();
         }
     } else {
         Notiflix.Notify.failure('Please enter a valid search query.');
     }
 });
 
-// Обработчик кнопки "Load more"
+
 loadMoreButton.addEventListener('click', async () => {
     const searchQuery = input.value.trim();
     currentPage++;
@@ -123,42 +110,11 @@ loadMoreButton.addEventListener('click', async () => {
 
             if (images.length > 0) {
                 createImageCards(images);
-                imageContainer.style.display = 'block';
-            } else {
-                toggleLoadMoreButton(false);
-                Notiflix.Notify.info("You've reached the end of search results.");
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            Notiflix.Notify.failure('An error occurred while fetching images. Please try again later.');
-        }
-    } else {
-        toggleLoadMoreButton(false);
-        Notiflix.Notify.info("You've reached the end of search results.");
-    }
-});
 
-// ...
-
-
-
-
-// Обработчик кліку на кнопку "Load more"
-loadMoreButton.addEventListener('click', async () => {
-    const searchQuery = input.value.trim();
-    currentPage++;
-
-    if (currentPage <= totalPages) {
-        try {
-            const searchData = await performImageSearch(searchQuery, currentPage);
-            const images = searchData.hits;
-
-            if (images.length > 0) {
-                createImageCards(images);
-                imageContainer.style.display = 'block';
-            } else {
-                toggleLoadMoreButton(false);
-                Notiflix.Notify.info("You've reached the end of search results.");
+                if (imagesLoaded >= searchData.totalHits) {
+                    toggleLoadMoreButton(false);
+                    Notiflix.Notify.info("You've reached the end of search results.");
+                }
             }
         } catch (error) {
             console.error('Error:', error);
